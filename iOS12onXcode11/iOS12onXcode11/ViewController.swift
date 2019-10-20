9d89cab8401ff7145c03cf40ff8270638f3b6c07
Haruhito Fuji <haruhito.fuji@gmail.com>
2019-10-20 13:36:26 +0900

    SingleViewApplicationを新規に作成

---
+++ iOS12onXcode11/iOS12onXcode11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS12onXcode11
+//
+//  Created by 藤　治仁 on 2019/10/20.
+//  Copyright © 2019 FromF.github.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
